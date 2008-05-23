6cd8efa7779cb3ee6121dbf7d988a46c053c481e
Christian Schmidt <schmidt@php.net>
2008-05-23 16:33:58 +0000

    avoid an infinite loop if OLE/ChainedBlockStream.php somehow is missing (bug 12921)
    git-svn-id: https://svn.php.net/repository/pear/packages/OLE/trunk@260165 c90b9560-bf6c-de11-be94-00142212c4b1

--- OLE.php
+++ OLE.php
@@ -194,6 +194,11 @@ class OLE extends PEAR
         $this->sbat = array();
         $shortBlockCount = $sbbatBlockCount * $this->bigBlockSize / 4;
         $sbatFh = $this->getStream($sbatFirstBlockId);
+        if (!$sbatFh) {
+            // Avoid an infinite loop if ChainedBlockStream.php somehow is
+            // missing
+            return false;
+        }
         for ($blockId = 0; $blockId < $shortBlockCount; $blockId++) {
             $this->sbat[$blockId] = $this->_readInt4($sbatFh);
         }
